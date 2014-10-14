9fce3d47aef42178cf75c9b1f2b6678dc95a452d
franzkel <linus.franzke@gmx.de>
2014-10-14 10:43:54 +0200

    git_intro - test update

--- git_intro/src/de/uni_koeln/spinfo/git_intro/Intro.java
+++ git_intro/src/de/uni_koeln/spinfo/git_intro/Intro.java
@@ -3,7 +3,11 @@ package de.uni_koeln.spinfo.git_intro;
 public class Intro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String hello = "Hello world";
+		
+		System.out.println(hello);
+		System.out.println("hello hello");
 
 	}
 
